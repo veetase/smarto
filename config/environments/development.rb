7a67ec9d0e47b46e6d78fc65d6c939ec581af920
wangguangxing <wangguangxing@bixuange.com>
2015-06-16 16:58:53 +0800

    1, add qiniu img cdn; 2, fix member card display issue on tablet;

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.assets.enabled = true
   config.active_support.deprecation = :log
-  config.action_controller.asset_host = nil
   config.assets.digest = true
   # Raise an error on page load if there are pending migrations.
   # config.active_record.migration_error = :page_load
