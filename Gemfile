9de89dcbb68c1e0a42d105c01950f8a108c94a9f
wangguangxing <wangguangxing@bixuange.com>
2015-07-07 18:08:32 +0800

    remake bixuange index page code

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem "devise-async"
 gem "httparty"
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem "compass-rails", github: "Compass/compass-rails", branch: "master"
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
-  config.assets.enabled = true
+  config.assets.enabled = false
   config.active_support.deprecation = :log
   #config.assets.digest = true
   # Raise an error on page load if there are pending migrations.
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  #config.assets.debug = true
+  config.assets.debug = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
